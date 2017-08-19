ae3fd5ef9b0ac86467dcfa9b255c63a99cdac334
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-08-19 13:03:56 +0200

    Tutorial checkpoint

--- FindArrayElements/Program.cs
+++ FindArrayElements/Program.cs
@@ -10,6 +10,30 @@ namespace FindArrayElements
     {
         static void Main(string[] args)
         {
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            //int indexOfTuesday = Array.IndexOf(daysOfWeek, "Tuesday");
+
+            ////searching array elements according to predicate bool condition
+            //int indexOfWednesday = Array.FindIndex(daysOfWeek, x => x[0] == 'W');
+            //Console.WriteLine(indexOfWednesday);
+            //Console.WriteLine(daysOfWeek[indexOfWednesday]);
+            //Console.WriteLine();
+
+            //finds all elements with 6 chars
+            string[] allWithSixChars = Array.FindAll(
+                daysOfWeek, x => x.Length == 6);
+            foreach (string item in allWithSixChars)
+                Console.WriteLine(item);
         }
     }
 }
